822109316e2d6a81e3d54222fa4727e0493e05f2
atsu666 <ito.atsu666@gmail.com>
2017-02-12 14:35:31 +0900

    bootstrap webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var path = require('path');
 var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/main.jsx',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
   resolve: {
@@ -26,7 +27,26 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader?modules', 'sass-loader']
+      }, {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './src/index.html'
+      }
+    ]),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,19 +1,42 @@
 var path = require('path');
 var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/main.jsx',
-  output: { path: path.join(__dirname, 'dist'), filename: 'bundle.js' },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        loader: ['babel-loader'],
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader?modules', 'sass-loader']
+      }, {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './src/index.html'
+      }
+    ]),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 }
