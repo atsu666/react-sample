ba2309c257333b5de29f6df3b6537dfaea4900f3
steelydylan <ess_president@me.com>
2017-02-13 10:07:14 +0900

    add createAction

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,5 @@
 import * as types from '../constants/ActionTypes';
-
-export const increment = () => ({ type: types.INCREMENT });
-
-export const decrement = () => ({ type: types.DECREMENT });
-
-export const rename = (name) => ({ type: types.RENAME, name: name });
+import { createAction } from 'redux-actions';
+export const increment = createAction(types.INCREMENT);
+export const decrement = createAction(types.DECREMENT);
+export const rename = createAction(types.RENAME);
